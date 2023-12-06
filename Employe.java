031f289307516c91dbe5c8671f4ca934e8fe10a4
AishwaryaTayde <152238067+AishwaryaTayde@users.noreply.github.com>
2023-12-06 14:16:18 +0530

    Employe name , id, salary sort using comparator

---
+++ Employe.java
@@ -0,0 +1,31 @@
+package com.TreeMap;
+
+import java.util.Comparator;
+
+public class Employe  {
+	int sid;
+	String Sname;
+	double Salary;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return Sname;
+	}
+	public void setSname(String sname) {
+		Sname = sname;
+	}
+	public double getSalary() {
+		return Salary;
+	}
+	public void setSalary(double salary) {
+		Salary = salary;
+	}
+	
+	
+	
+
+}
